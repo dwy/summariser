a7cf7b6d5471843f8df87d5bf0fdd89ac98e1cc9
dwy <dominic.wyler@zuehlke.com>
2014-11-12 20:24:41 +0100

    added default controller to debug azure problem

---
+++ Summariser/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Summariser.Data;
+using Summariser.Models;
+
+namespace Summariser.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public object Get()
+        {
+            return new int[] {1,2,3,4,5};
+        }
+
+        public object Get(int id)
+        {
+            return new { id = id, value = "default"};
+        }
+    }
+}
